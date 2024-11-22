a944b8e8534434478b217d3989b57d44d7ec5327
saniyaNLP <saniya.naphade@gumgum.com>
2024-11-22 11:12:25 -0700

    adding new functionality

--- campaign.py
+++ campaign.py
@@ -7,45 +7,16 @@ import pickle
 import os
 
 def display_compaign_topics():
-    st.title("Topic insights for campaign id: 17074")
-    df = pd.read_csv("s3://gumgum-research-sx/sn-models/category_entry_points/niche_anomaly/adam_spike/iab_index_values.csv")
-    df = df.sort_values(by = ['Topic']).reset_index(drop=True)
-    st.dataframe(df)
+    if "topics_df" not in st.session_state.keys():
+        df =pd.read_csv("s3://gumgum-research-sx/sn-models/category_entry_points/niche_anomaly/adam_spike/iab_index_values.csv")
+        st.session_state['topics_df'] = df
 
-    time_series_df = pd.read_csv("s3://gumgum-research-sx/sn-models/category_entry_points/niche_anomaly/adam_spike"
+    if "time_series_df" not in st.session_state.keys():
+        df =pd.read_csv("s3://gumgum-research-sx/sn-models/category_entry_points/niche_anomaly/adam_spike"
                                  "/avg_attention_time_series.csv")
-    st.subheader('AVERAGE VIEWABLE_ATTENTION_EVENTS Time Series')
+        st.session_state['time_series_df'] = df
 
-    all_topics = ['All'] + list(time_series_df['Name'].unique())
-    selected_topic = st.selectbox('Select a topic:', all_topics)
+    st.title("Topic insights for campaign id: 17074")
 
-    columns = ['VIEWABLE_ATTENTION_EVENTS', 'AVG_VIEWABLE_ATTENTION_TIME', 'CTR', 'COUNT']
-    selected_column = st.selectbox('Select a column to visualize:', columns)
-
-    # Filter the dataframe based on the selected product
-    if selected_topic == 'All':
-        filtered_df = time_series_df
-    else:
-        filtered_df = time_series_df[time_series_df['Name'] == selected_topic]
-
-    # Plot the price time series
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    if selected_topic == 'All':
-        # If 'All' is selected, plot all products in the same graph with hover tooltips
-        fig = px.line(time_series_df, x='DAY', y=selected_column, color='Topic', title="AVERAGE "
-                                                                                    f"{selected_column} for "
-                                                                                       f"All Topics",
-                      labels={selected_column: f'AVG {selected_column}', 'DAY': 'Date'})
-    else:
-        # If a specific product is selected, plot only that product
-        fig = px.line(filtered_df, x='DAY', y=selected_column, title=f"AVERAGE {selected_column} "
-                                                                               f"for topic"
-                                                              f" {selected_topic}",
-                      labels={selected_column: f'AVG {selected_column}', 'DAY': 'Date'})
-
-    # Update layout to remove the legend (optional)
-    fig.update_layout(showlegend=False)
-
-    # Display the plot
-    st.plotly_chart(fig)
+    st.session_state['topics_df'] = st.session_state['topics_df'].sort_values(by = ['Topic']).reset_index(drop=True)
+    st.dataframe(st.session_state['topics_df'])
---
+++ graphs.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import plotly.express as px
+
+
+def display_compaign_topics():
+    st.title("Topic insights for campaign id: 17074")
+
+    time_series_df = st.session_state['time_series_df']
+
+    tab1, tab2 = st.tabs("Time Series", "Topics")
+    with tab1:
+        st.subheader('Time Series')
+
+        all_topics = ['All'] + list(time_series_df['Name'].unique())
+        selected_topic = st.selectbox('Select a topic:', all_topics)
+
+        columns = ['VIEWABLE_ATTENTION_EVENTS', 'AVG_VIEWABLE_ATTENTION_TIME', 'CTR', 'COUNT']
+        selected_column = st.selectbox('Select a column to visualize:', columns)
+
+        # Filter the dataframe based on the selected product
+        if selected_topic == 'All':
+            filtered_df = time_series_df
+        else:
+            filtered_df = time_series_df[time_series_df['Name'] == selected_topic]
+
+        # Plot the price time series
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        if selected_topic == 'All':
+            # If 'All' is selected, plot all products in the same graph with hover tooltips
+            fig = px.line(time_series_df, x='DAY', y=selected_column, color='Topic', title="AVERAGE "
+                                                                                        f"{selected_column} for "
+                                                                                           f"All Topics",
+                          labels={selected_column: f'AVG {selected_column}', 'DAY': 'Date'})
+        else:
+            # If a specific product is selected, plot only that product
+            fig = px.line(filtered_df, x='DAY', y=selected_column, title=f"AVERAGE {selected_column} "
+                                                                                   f"for topic"
+                                                                  f" {selected_topic}",
+                          labels={selected_column: f'AVG {selected_column}', 'DAY': 'Date'})
+
+        # Update layout to remove the legend (optional)
+        fig.update_layout(showlegend=False)
+
+        # Display the plot
+        st.plotly_chart(fig)
+
+    with tab2:
+        topics_df = st.session_state['topics_df']
+        st.subheader('Time Series')
+
+        all_topics = ['All'] + list(topics_df['Name'].unique())
+        selected_topic = st.selectbox('Select a topic:', all_topics)
+
+        columns = ['VIEWABLE_ATTENTION_EVENTS', 'AVG_VIEWABLE_ATTENTION_TIME', 'CTR', 'COUNT']
+        selected_column = st.selectbox('Select a column to visualize:', columns)
+
+        # Filter the dataframe based on the selected product
+        if selected_topic == 'All':
+            filtered_df = topics_df
+        else:
+            filtered_df = topics_df[topics_df['Name'] == selected_topic]
+
+        # Plot the price time series
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        if selected_topic == 'All':
+            # If 'All' is selected, plot all products in the same graph with hover tooltips
+            fig = px.line(time_series_df, x='Topic', y=selected_column, title="AVERAGE "
+                                                                                           f"{selected_column} for "
+                                                                                           f"All Topics",
+                          labels={selected_column: f'AVG {selected_column}', 'DAY': 'Date'})
+        else:
+            # If a specific product is selected, plot only that product
+            filtered_df = filtered_df.sort_values(by = selected_column)
+            fig = px.line(filtered_df, x='Topic', y=selected_column, title=f"AVERAGE {selected_column} "
+                                                                         f"for topic"
+                                                                         f" {selected_topic}",
+                          labels={selected_column: f'AVG {selected_column}', 'DAY': 'Date'})
+
+        # Update layout to remove the legend (optional)
+        fig.update_layout(showlegend=False)
+
+        # Display the plot
+        st.plotly_chart(fig)
+
