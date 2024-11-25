71f4ed28f4d3207f5c80218349fb578523fb38fa
saniyaNLP <saniya.naphade@gumgum.com>
2024-11-25 11:16:51 -0700

    adding per iab topics

--- iab_class.py
+++ iab_class.py
@@ -2,21 +2,19 @@ import streamlit as st
 import pandas as pd
 from graphs import plot_graphs
 import boto3
-import pickle
 
-s3 = boto3.resource('s3')
-bucket = 'gumgum-research-sx'
 def display_iab_topics():
-    key = 'sn-models/category_entry_points/niche_anomaly/adam_spike/per_iab/overall_results.pkl'
     if "overall_results" not in st.session_state.keys():
-        overall_results = pickle.loads(s3.Bucket(bucket).Object(key).get()['Body'].read())
-        st.session_state['overall_results'] = overall_results
+        df = pd.read_csv("s3://gumgum-research-sx/sn-models/category_entry_points/niche_anomaly/adam_spike/per_iab"
+               "/time_series_scores.csv")
+        st.session_state['overall_results'] = df
     iabv3_classes = list(st.session_state['overall_results'].keys())
     selected_cat = st.selectbox('Select an IAB cat:', iabv3_classes)
 
     st.title("Topic insights for campaign id: 17074 per IAB category")
 
-    selected_df = st.session_state['overall_results'][selected_cat]
+    selected_df = st.session_state['overall_results'][st.session_state['overall_results'][
+                                                          'iab_cat']==selected_cat].copy()
 
     selected_df = selected_df.sort_values(by = ['Topic']).reset_index(drop=True)
     st.dataframe(selected_df)
